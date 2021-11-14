a7b818757e2f122498ab888d060c9132fe7cb6d4
david-uash <58273225+david-uash@users.noreply.github.com>
2021-11-14 11:06:00 +0200

    Update myscript.py

--- myscript.py
+++ myscript.py
@@ -5,11 +5,6 @@ import subprocess
 import sys
 
 
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-
-install("numpy")
 import numpy
 a = np.zeros(2,2)
 print(a)
